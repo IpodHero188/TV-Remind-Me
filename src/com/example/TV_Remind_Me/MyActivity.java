0e2463c22488c1cf55beda363b839dbb00e7adb1
IpodHero188 <DanielMaclin@gmail.com>
2014-06-12 18:26:08 -0500

    The changes I started working on for the main page

--- src/com/example/TV_Remind_Me/MyActivity.java
+++ src/com/example/TV_Remind_Me/MyActivity.java
@@ -5,12 +5,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import java.util.HashMap;
+
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
     boolean populatedShows = false; //Checks if the user has any saved shows
     boolean has_list = false;//Checks to see if there is a profile attached
+    HashMap <Integer, Integer> tempShowStorage;
+    HashMap <Integer, Boolean> tempEpisodeStorage;
     Button MenuButton;
 
     @Override
@@ -35,11 +39,19 @@ public class MyActivity extends Activity {
             /*
                 load the tv shows into the gridview
              */
+            tempShowStorage = new HashMap<Integer, Integer>();
+            tempEpisodeStorage = new HashMap<Integer, Boolean>();
+
         }
         else{
             //The user does not have a established profile
             Intent registerIntent = new Intent(this, Registration.class);
             startActivity(registerIntent);
         }
+    }
+
+    //during inactivity, the data from the hashmaps will be written to the file
+    protected void onPause(){
+
     }
 }
